3a331b1d06fbf01ef5f85665023151b813efdbac
NoughtG <hod.datubo@gmail.com>
2025-07-29 12:15:06 +0100

    admin, member

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -20,3 +20,29 @@ urlpatterns = [
 #     path('books/', list_books, name='list_books'),
 #     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 # ]
+
+from django.urls import path
+from .views import (
+    admin_view,
+    librarian_view,
+    member_view,
+    list_books,
+    LibraryDetailView,
+    register,
+    user_login,
+    user_logout
+)
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
+
+    path('admin-dashboard/', admin_view, name='admin_view'),
+    path('librarian-dashboard/', librarian_view, name='librarian_view'),
+    path('member-dashboard/', member_view, name='member_view'),
+
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,12 +3,36 @@
 # Create your views here.
 
 from .models import Library
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.shortcuts import render
 
 from django.views.generic.detail import DetailView
 
 from django.shortcuts import render, get_object_or_404
 # from django.views.generic import DetailView
 # from .models import Book, Library
+
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
 
 # Function-based view to list all books
 def list_books(request):
