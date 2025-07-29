b2b7b944214611ad6f3f94194d479f43ec401b8d
NoughtG <hod.datubo@gmail.com>
2025-07-29 15:02:16 +0100

    Implementing a custom User Model

---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,57 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView
+# from django.urls import path
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+#    path('login/', views.user_login, name='login'),
+#    path('logout/', views.user_logout, name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    path('register/', views.register, name='register'),
+    
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
+
+# urlpatterns = [
+#     path('books/', list_books, name='list_books'),
+#     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+# ]
+
+from django.urls import path
+from .views import (
+    admin_view,
+    librarian_view,
+    member_view,
+    list_books,
+    LibraryDetailView,
+    register,
+    user_login,
+    user_logout
+)
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
+
+    path('admin-dashboard/', admin_view, name='admin_view'),
+    path('librarian-dashboard/', librarian_view, name='librarian_view'),
+    path('member-dashboard/', member_view, name='member_view'),
+
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
+
+from .views import add_book, edit_book, delete_book
+
+urlpatterns += [
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
+]
+
