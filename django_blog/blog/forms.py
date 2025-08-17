fed480eb8658074614ea0a6a9877f5f6dba010d4
NoughtG <125405940+NoughtG@users.noreply.github.com>
2025-08-17 13:55:58 +0100

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Comment
+from taggit.forms import TagWidget 
+from .models import Comment, Post    
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -12,7 +13,7 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ("username", "email", "password1", "password2")
 
 
-class CommentForm(forms.ModelForm):   # ✅ required by checker
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
@@ -20,4 +21,13 @@ class CommentForm(forms.ModelForm):   # ✅ required by checker
             "content": forms.Textarea(
                 attrs={"rows": 3, "placeholder": "Write a comment..."}
             )
+        }
+
+
+class PostForm(forms.ModelForm):  
+    class Meta:
+        model = Post
+        fields = ["title", "content", "tags"]
+        widgets = {
+            "tags": TagWidget(),  
         }
